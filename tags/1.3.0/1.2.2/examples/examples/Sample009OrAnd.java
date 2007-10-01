7f299638190ef7386edd02c5a61cb938b959a40c
derek.mahar <derek.mahar@bb9efd34-2b2c-0410-b9d9-3dd60dd515ea>
2007-10-01 20:46:50 +0000

    Released Squiggle 1.3.0.

---
+++ tags/1.3.0/1.2.2/examples/examples/Sample009OrAnd.java
@@ -0,0 +1,33 @@
+package examples;
+
+import com.truemesh.squiggle.*;
+
+public class Sample009OrAnd {
+    public static void main(String[] args) {
+
+        Table user = new Table("user");
+
+        SelectQuery select = new SelectQuery(user);
+
+        select.addColumn(new WildCardColumn(user));
+
+        Criteria name = new MatchCriteria(user, "name", MatchCriteria.LIKE, "a%");
+        Criteria id = new MatchCriteria(user, "id", MatchCriteria.EQUALS, 12345);
+        Criteria feet = new MatchCriteria(user, "feet", MatchCriteria.EQUALS, "smelly");
+
+        select.addCriteria(new OR(name, new AND(id, feet)));
+
+        System.out.println(select);
+
+    }
+
+}
+
+// Result:
+//
+// SELECT
+//     user.*
+// FROM
+//     user
+// WHERE
+//     ( user.name LIKE 'a%' OR ( user.id = 12345 AND user.feet = 'smelly' ) )
---
+++ tags/1.3.0/1.2.3/examples/examples/Sample001SimplestSelect.java
@@ -0,0 +1,32 @@
+package examples;
+
+import com.truemesh.squiggle.SelectQuery;
+import com.truemesh.squiggle.Table;
+import com.truemesh.squiggle.Order;
+
+public class Sample001SimplestSelect {
+    public static void main(String[] args) {
+
+        Table people = new Table("people");
+
+        SelectQuery select = new SelectQuery(people);
+
+        select.addColumn(people, "firstname");
+        select.addColumn(people, "lastname");
+
+        select.addOrder(people, "age", Order.DESCENDING);
+
+        System.out.println(select);
+
+    }
+}
+
+// Result:
+//
+// SELECT
+//    people.firstname ,
+//    people.lastname
+// FROM
+//     people
+// ORDER BY
+//     people.age DESC
