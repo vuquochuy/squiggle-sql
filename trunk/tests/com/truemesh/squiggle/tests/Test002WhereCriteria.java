110109feb74870acb8ae21909e6c53fba3c9b135
nat.pryce <nat.pryce@bb9efd34-2b2c-0410-b9d9-3dd60dd515ea>
2008-10-02 09:52:35 +0000

    more tests, fixed MatchCriteria constructors

--- trunk/tests/com/truemesh/squiggle/tests/Test002WhereCriteria.java
+++ trunk/tests/com/truemesh/squiggle/tests/Test002WhereCriteria.java
@@ -1,9 +1,7 @@
 package com.truemesh.squiggle.tests;
 
-import com.truemesh.squiggle.InCriteria;
-import com.truemesh.squiggle.MatchCriteria;
-import com.truemesh.squiggle.SelectQuery;
-import com.truemesh.squiggle.Table;
+import com.truemesh.squiggle.*;
+import com.truemesh.squiggle.literal.FloatLiteral;
 import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
@@ -18,10 +16,11 @@ public class Test002WhereCriteria {
         select.addColumn(people, "lastname");
 
         select.addCriteria(
-                new MatchCriteria(people, "age", MatchCriteria.GREATER, "30"));
-
+                new MatchCriteria(people, "height", MatchCriteria.GREATER, 1.8));
         select.addCriteria(
                 new InCriteria(people, "department", new String[] {"I.T.", "Cooking"}));
+        select.addCriteria(
+                new BetweenCriteria(people.getColumn("age"), 18, 30));
 
         assertThat(select, SqlMatcher.generatesSql(
                 "SELECT " +
@@ -30,10 +29,11 @@ public class Test002WhereCriteria {
                 "FROM " +
                 "    people " +
                 "WHERE " +
-                "    people.age > '30' AND " +
+                "    people.height > 1.8 AND " +
                 "    people.department IN ( " +
                 "        'I.T.','Cooking' " +
-                ")"));
+                "    ) AND" +
+                "    people.age BETWEEN 18 AND 30"));
 
     }
 }
